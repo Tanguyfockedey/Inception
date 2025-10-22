8905d92bc9fc31e92b6d4bffc4781c3f65137351
tafocked <tafocked@inception>
2025-10-22 12:31:18 +0000

    wp working but maybe not php ? Or CSS ?

--- srcs/requirements/mariadb/conf/docker-entrypoint.sh
+++ srcs/requirements/mariadb/conf/docker-entrypoint.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 set -e
 
-echo "Starting docker-entrypoint.sh for MariaDB"
+log() { echo "[$(date +'%F %T')] $*"; }
+log "Starting docker-entrypoint.sh for MariaDB"
 
 # Load environment variables from secrets
-echo "Loading environment variables from secrets..."
+log "Loading environment variables from secrets..."
 export DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
 export DB_PASSWORD=$(cat /run/secrets/db_password)
 
 # Initialize DB if empty
-echo "Checking if database is initialized..."
+log "Checking if database is initialized..."
 if [ ! -d /var/lib/mysql/mysql ]; then
     mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 
 # Start MariaDB in background
-echo "Starting temporary MariaDB server..."
+log "Starting temporary MariaDB server..."
 mariadbd-safe --skip-networking &
 pid="$!"
 
@@ -22,48 +23,50 @@ pid="$!"
 until mariadb-admin ping --silent; do
     sleep 1
 done
-echo "Waiting for MariaDB to start..."
+log "Waiting for MariaDB to start..."
 
 # Secure MariaDB installation
-echo "Securing MariaDB installation..."
+log "Securing MariaDB installation..."
 
 # Set root password
-echo "Setting root password..."
+log "Setting root password..."
 mariadb -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';"
 
 # Remove anonymous users
-echo "Removing anonymous users..."
+log "Removing anonymous users..."
 mariadb -u root -p"$DB_ROOT_PASSWORD" -e "DELETE FROM mysql.user WHERE User='';"
 
 # Disallow remote root login
-echo "Disallowing remote root login..."
+log "Disallowing remote root login..."
 mariadb -u root -p"$DB_ROOT_PASSWORD" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
 
 # Remove test database
-echo "Removing test database..."
+log "Removing test database..."
 mariadb -u root -p"$DB_ROOT_PASSWORD" -e "DROP DATABASE IF EXISTS test;"
 mariadb -u root -p"$DB_ROOT_PASSWORD" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
 
 # Flush privileges
-echo "Flushing privileges..."
+log "Flushing privileges..."
 mariadb -u root -p"$DB_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
 
 # Create the specified database
-echo "Creating database..."
+log "Creating database..."
 mariadb -u root -p"$DB_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS $DB;"
 
 # Create a user and grant privileges
-echo "Creating user and granting privileges..."
+log "Creating user and granting privileges..."
 mariadb -u root -p"$DB_ROOT_PASSWORD" -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
 mariadb -u root -p"$DB_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON $DB.* TO '$DB_USER'@'%';"
 mariadb -u root -p"$DB_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
 
 # Stop the temporary MariaDB server
-echo "Stopping temporary MariaDB server..."
+log "Stopping temporary MariaDB server..."
 mariadb-admin -u root -p"${DB_ROOT_PASSWORD}" shutdown
 
+# Wait for the background process to finish
+wait "$pid"
 fi
 
 # Finally, exec main process (MariaDB server)
-echo "Starting MariaDB server..."
+log "Starting MariaDB server..."
 exec "$@"
--- srcs/requirements/wordpress/conf/docker-entrypoint.sh
+++ srcs/requirements/wordpress/conf/docker-entrypoint.sh
@@ -3,6 +3,9 @@ set -e
 
 echo "Starting docker-entrypoint.sh for WordPress..."
 
+# addgroup -S www-data
+adduser -S -G www-data www-data
+
 export DB_PASSWORD=$(cat /run/secrets/db_password)
 export WP_ADMIN_PASSWORD=$(cat /run/secrets/wp_admin_password)
 export WP_USER_PASSWORD=$(cat /run/secrets/wp_user_password)
@@ -15,22 +18,37 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
-if [ ! -f wp-config-sample.php ]; then
+echo "Waiting for MariaDB..."
+until mysql -hmariadb -u"$DB_USER" -p"$DB_PASSWORD" -e "SELECT 1" >/dev/null 2>&1; do
+	sleep 1
+done
+
+if [ ! -f wp-config.php ]; then
 	echo "Downloading WordPress..."
 	php -d memory_limit=512M /usr/local/bin/wp core download --allow-root --locale=en_US
 
-	sleep 30
-
 	echo "Creating custom wp-config.php..."
 	wp config create --allow-root --dbname=$DB --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=mariadb
+
+	# # Enable WP debug logging
+	# wp config set WP_DEBUG true --raw --allow-root
+	# wp config set WP_DEBUG_LOG true --raw --allow-root
+	# wp config set WP_DEBUG_DISPLAY false --raw --allow-root
+
+	# wp option update siteurl "https://localhost:4343" --allow-root --path=/var/www/html
+	# wp option update home "https://localhost:4343" --allow-root --path=/var/www/html
+
+	# echo "if (isset(\$_SERVER['HTTP_X_FORWARDED_PROTO']) && \$_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {\$_SERVER['HTTPS'] = 'on';}" >> /var/www/html/wp-config.php
+
 	echo "Installing WordPress..."
 	wp core install --allow-root --url=$DOMAIN --title="Inception" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email="admin@admin.com"
+
 	echo "Creating additional user..."
 	wp user create --allow-root $WP_USER "author@author.com" --user_pass=$WP_USER_PASSWORD --role=author
 
 fi
 # Fix permissions for the worpress directory
-#chown -R www-data:www-data /var/www/html
+chown -R www-data:www-data /var/www/html
 chmod -R 777 /var/www/html
 
 exec "$@"
